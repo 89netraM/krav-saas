a77aa1ab23924f8632bae2d9b1529fdc1d83dbdf
Daniel Torres <daniel.torres@cygni.se>
2025-01-24 18:16:00 +0100

    ghost spinner

--- krav-saas/src/App.tsx
+++ krav-saas/src/App.tsx
@@ -1,11 +1,12 @@
 import "./index.css";
+
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-content-center items-center h-screen">
-        <h1> Krav SaaS </h1>
+      <div className="flex flex-col justify-content-center items-center h-100">
+        <h1> Requirements SaaS </h1>
 
-        <h2> Titel: </h2>
+        <h2> Title: </h2>
         <div className="flex align items-baseline gap-4">
           <p> As a </p>
           <select className="mt-4 p-2 border rounded">
@@ -33,8 +34,14 @@ function App() {
           </select>
         </div>
         <p className="font-bold"> Priority : 1 </p>
-        <button className="bg-green-400 text-white font-bold py-2 px-4 rounded hover:bg-green-500 transition duration-300"> Reroll (0.19$) </button>
-        <button className="bg-red-400 text-white font-bold py-2 px-4 rounded hover:bg-green-500 transition duration-300"> Boost (0.99$)</button>
+        <button className="bg-green-400 text-white font-bold py-2 px-4 rounded hover:bg-green-500 transition duration-300">
+          {" "}
+          Reroll (0.19$){" "}
+        </button>
+        <button className="bg-red-400 text-white font-bold py-2 px-4 rounded hover:bg-green-500 transition duration-300">
+          {" "}
+          Boost (0.99$)
+        </button>
       </div>
     </>
   );
---
+++ krav-saas/src/components/Spinner/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function Spinner() {
+  return (
+    <div className="container">
+      <div className="ring" />
+    </div>
+  );
+}
+
+export default Spinner;
